0108abfee9f1672b84c7cb1feec94dcb81495dc1
Hypersand <on8214@gmail.com>
2023-03-20 14:29:54 +0900

    2-04 엔티티

---
+++ src/main/java/com/mysite/sbb/entity/Question.java
@@ -0,0 +1,30 @@
+package com.mysite.sbb.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String subject;
+
+    private String content;
+
+    private LocalDateTime createData;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+
+
+}
